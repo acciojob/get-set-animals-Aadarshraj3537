cdefa9e981f4354707a5a1fcc3b6c06e010975e3
Aadarsh Raj <aadarshraj351@gmail.com>
2025-02-11 11:42:23 +0530

    created a submission

--- script.js
+++ script.js
@@ -9,7 +9,6 @@ class Animal {
   }
 }
 
-// Define the Dog class that extends Animal
 class Dog extends Animal {
   constructor(species) {
     super(species);
@@ -20,7 +19,6 @@ class Dog extends Animal {
   }
 }
 
-// Define the Cat class that extends Animal
 class Cat extends Animal {
   constructor(species) {
     super(species);
@@ -31,7 +29,6 @@ class Cat extends Animal {
   }
 }
 
-// Do not change the code below this line
 window.Animal = Animal;
 window.Dog = Dog;
 window.Cat = Cat;
